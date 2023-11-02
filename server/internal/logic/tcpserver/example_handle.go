f7217a9812a7c035d6bdc70e8117b258bd7b16db
wsq <wsq990906@gmail.com>
2023-11-02 17:36:37 +0800

    tcpserver国际化

--- server/internal/logic/tcpserver/example_handle.go
+++ server/internal/logic/tcpserver/example_handle.go
@@ -26,12 +26,12 @@ func (s *sTCPServer) OnExampleHello(ctx context.Context, req *servmsg.ExampleHel
 	}
 
 	if conn.Auth == nil {
-		res.SetError(gerror.New("连接未认证，请确认已登录成功！"))
+		res.SetError(gerror.New(g.I18n().T(ctx, "{#ConnectNoAuthority}")))
 		_ = conn.Send(ctx, res)
 		return
 	}
 
-	data.Desc = fmt.Sprintf("Hello %v, 你的APPID：%v，当前Grata版本：%v，你成功请求了`servmsg.ExampleHelloReq`接口！", req.Name, conn.Auth.AppId, consts.VersionApp)
+	data.Desc = g.I18n().Tf(ctx, "{#ExampleHelloReq}", req.Name, conn.Auth.AppId, consts.VersionApp)
 	data.Timestamp = gtime.Now()
 	res.Data = data
 	_ = conn.Send(ctx, res)
@@ -40,7 +40,7 @@ func (s *sTCPServer) OnExampleHello(ctx context.Context, req *servmsg.ExampleHel
 // OnExampleRPCHello 一个rpc请求例子
 func (s *sTCPServer) OnExampleRPCHello(ctx context.Context, req *servmsg.ExampleRPCHelloReq) (res *servmsg.ExampleRPCHelloRes, err error) {
 	var data = new(servmsgin.ExampleHelloModel)
-	data.Desc = fmt.Sprintf("Hello %v, 当前Grata版本：%v，你成功请求了`servmsg.ExampleRPCHelloReq`接口！", req.Name, consts.VersionApp)
+	data.Desc = fmt.Sprintf(g.I18n().Tf(ctx, "{#ExampleRPCHelloReq}"), req.Name, consts.VersionApp)
 	data.Timestamp = gtime.Now()
 
 	res = new(servmsg.ExampleRPCHelloRes)
