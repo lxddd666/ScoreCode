be75dca414d78582cd3824508cac1c042f42ec93
luxuedong <3138689283@qq.com>
2023-11-27 19:17:13 +0800

    分组

---
+++ web/src/views/tg/tgFolders/edit.vue
@@ -0,0 +1,143 @@
+<template>
+  <div>
+    <n-spin :show="loading" description="请稍候...">
+      <n-modal
+        v-model:show="isShowModal"
+        :show-icon="false"
+        preset="dialog"
+        :title="params?.id > 0 ? '编辑 #' + params?.id : '添加'"
+        :style="{
+          width: dialogWidth,
+        }"
+      >
+        <n-form
+          :model="params"
+          :rules="rules"
+          ref="formRef"
+          label-placement="left"
+          :label-width="200"
+          class="py-4"
+        >
+          <n-form-item label="组织ID" path="orgId">
+            <n-input-number placeholder="请输入组织ID" v-model:value="params.orgId" />
+          </n-form-item>
+
+          <n-form-item label="用户ID" path="memberId">
+            <n-input-number placeholder="请输入用户ID" v-model:value="params.memberId" />
+          </n-form-item>
+
+          <n-form-item label="分组名称" path="folderName">
+            <n-input placeholder="请输入分组名称" v-model:value="params.folderName" />
+          </n-form-item>
+
+          <n-form-item label="账号" path="accounts">
+            <n-select multiple v-model:value="params.accounts" :options="options.accounts" />
+          </n-form-item>
+
+          <n-form-item label="备注" path="comment">
+            <n-input type="textarea" placeholder="备注" v-model:value="params.comment" />
+          </n-form-item>
+        </n-form>
+        <template #action>
+          <n-space>
+            <n-button @click="closeForm">取消</n-button>
+            <n-button type="info" :loading="formBtnLoading" @click="confirmForm">确定</n-button>
+          </n-space>
+        </template>
+      </n-modal>
+    </n-spin>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { onMounted, ref, computed, watch } from 'vue';
+  import { Edit, View } from '@/api/tg/tgFolders';
+  import { rules, options, State, newState } from './model';
+  import { useMessage } from 'naive-ui';
+  import { adaModalWidth } from '@/utils/hotgo';
+
+  const emit = defineEmits(['reloadTable', 'updateShowModal']);
+
+  interface Props {
+    showModal: boolean;
+    formParams?: State;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    showModal: false,
+    formParams: () => {
+      return newState(null);
+    },
+  });
+
+  const isShowModal = computed({
+    get: () => {
+      return props.showModal;
+    },
+    set: (value) => {
+      emit('updateShowModal', value);
+    },
+  });
+
+  const loading = ref(false);
+  const params = ref<State>(props.formParams);
+  const message = useMessage();
+  const formRef = ref<any>({});
+  const dialogWidth = ref('75%');
+  const formBtnLoading = ref(false);
+
+  function confirmForm(e) {
+    e.preventDefault();
+    formBtnLoading.value = true;
+    formRef.value.validate((errors) => {
+      if (!errors) {
+        Edit(params.value).then((_res) => {
+          message.success('操作成功');
+          setTimeout(() => {
+            isShowModal.value = false;
+            emit('reloadTable');
+          });
+        });
+      } else {
+        message.error('请填写完整信息');
+      }
+      formBtnLoading.value = false;
+    });
+  }
+
+  onMounted(async () => {
+    adaModalWidth(dialogWidth);
+  });
+
+  function closeForm() {
+    isShowModal.value = false;
+  }
+
+  function loadForm(value) {
+    // 新增
+    if (value.id < 1) {
+      params.value = newState(value);
+      loading.value = false;
+      return;
+    }
+
+    loading.value = true;
+    // 编辑
+    View({ id: value.id })
+      .then((res) => {
+        params.value = res;
+      })
+      .finally(() => {
+        loading.value = false;
+      });
+  }
+
+  watch(
+    () => props.formParams,
+    (value) => {
+      loadForm(value);
+    }
+  );
+</script>
+
+<style lang="less"></style>
