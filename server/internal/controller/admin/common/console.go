219b075ceeaa82fa252a32113e18701ca2d37747
lxd <3138689283@qq.com>
2023-12-27 11:41:56 +0800

    console

--- server/internal/controller/admin/common/console.go
+++ server/internal/controller/admin/common/console.go
@@ -17,6 +17,7 @@ import (
 	"hotgo/internal/model/entity"
 	"hotgo/internal/model/input/commonin"
 	"math"
+	"time"
 )
 
 var Console = cConsole{}
@@ -116,19 +117,67 @@ func GetPrometheus(ctx context.Context, name string, params map[string]interface
 	url := fmt.Sprintf("%s/api/v1/query?query=%s%s", prometheus_url, name, param)
 	//url := "http://localhost:9090/api/v1/query?query=prometheus_http_requests_total{code='200',handler='/manifest.json'}"
 	resp := g.Client().Discovery(nil).GetContent(ctx, url)
-	promResp := entity.PrometheusResponse{}
-	err = json.Unmarshal([]byte(resp), &promResp)
-	if err != nil {
-		return
-	}
-	if promResp.Status == "success" {
-		value := promResp.Data.Result[0].Value[1]
-		if value != nil {
-			model.Number = gconv.Int64(value)
-		} else {
-			model.Number = 0
+	if resp != "" {
+		promResp := entity.PrometheusResponse{}
+		err = json.Unmarshal([]byte(resp), &promResp)
+		if err != nil {
+			return
 		}
+		if promResp.Status == "success" {
+			value := promResp.Data.Result[0].Value[1]
+			if value != nil {
+				model.Number = gconv.Int64(value)
+			} else {
+				model.Number = 0
+			}
+		}
+		model.Statue = promResp.Status
+	} else {
+		model.Number = 0
 	}
-	model.Statue = promResp.Status
+	return
+}
+
+// GetDatePrometheus 获取当日的添加量
+func GetDatePrometheus(ctx context.Context, name string, params map[string]interface{}) (err error, model commonin.PrometheusResponseModel) {
+	now := time.Now()
+	// 将当前时间转换为当天的凌晨0点
+	midnight := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
+	nextMidNight := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
+	// 格式化时间为 ISO 8601 格式
+	start := midnight.Format(time.RFC3339)
+	end := nextMidNight.Format(time.RFC3339)
+
+	prometheus_url := g.Cfg().MustGet(ctx, "prometheus.address").String()
+	param := ""
+	for k, v := range params {
+		p := k + "='" + gconv.String(v) + "'"
+		param += p
+		param += ","
+	}
+	if param != "" {
+		param = "{" + param + "}"
+	}
+	url := fmt.Sprintf("%s/api/v1/query_range?query=%s%s&start=%s&end=%s&step=%dm", prometheus_url, name, param, start, end, 3)
+	//url := "http://localhost:9090/api/v1/query_range?query=tg_add_group_member{group=%22%22,}&start=2023-12-27T00:00:00Z&end=2023-12-28T00:00:00Z&step=5m"
+	resp := g.Client().Discovery(nil).GetContent(ctx, url)
+	if resp != "" {
+		promResp := entity.PrometheusResponse{}
+		err = json.Unmarshal([]byte(resp), &promResp)
+		if err != nil {
+			return
+		}
+		if promResp.Status == "success" {
+			value := promResp.Data.Result[0].Value[1]
+			if value != nil {
+				model.Number = gconv.Int64(value)
+			} else {
+				model.Number = 0
+			}
+		}
+		model.Statue = promResp.Status
+	} else {
+		model.Number = 0
+	}
 	return
 }
